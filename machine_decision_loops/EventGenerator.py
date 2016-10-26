54537caddd616ba6d5001bd895850afe6c0aeaa1
Nikos Daniilidis <nikos.daniilidis@gmail.com>
2016-10-25 22:16:35 -0700

    Peppered with comments.

--- machine_decision_loops/EventGenerator.py
+++ machine_decision_loops/EventGenerator.py
@@ -4,16 +4,49 @@ from scipy.stats import chi2
 __author__ = "nikos.daniilidis"
 
 class EventGenerator:
-
-    def __init__(self, seed=42, num_inputs=11, type='chisq', balance=0.5):
+    """
+    Class which creates a stream of events. Each event is an input vector and a label
+    correspondng to the input vector. Certain categories of events with known statistical
+    properties are implemented
+    """
+    def __init__(self, seed=42, num_inputs=11, kind='chisq', balance=0.5):
+        """
+        Initialize the instance
+        :param seed: Int. The seed for the numpy random state.
+        :param num_inputs: Int. Number of inputs in the input vector of each event.
+        :param kind:  String. Type of event stream to generate.
+        :param balance: Float between 0. and 1.0. Fraction of the 0 class in the event stream
+        :return: None
+        """
+        assert isinstance(seed, int)
+        assert isinstance(num_inputs, int)
+        assert kind in ('chisq')
+        assert isinstance(balance, float)
+        assert (balance >= 0.) and (balance <= 1.)
         self.seed = seed
         np.random.seed(seed)
         self.num_inputs = num_inputs
-        self.type = type
-        if type == 'chisq':
+        self.kind = kind
+        if kind == 'chisq':
             self.cutoff = chi2.isf(balance, df=num_inputs)
 
-    def get_chisq(self, num_events=100):
+    def get(self, num_events=100):
+        """
+        Generate a number of events and their labels
+        :param num_events: Int. Number of events to generate
+        :return: x, y. Numpy arrays of float/int
+        """
+        if self.kind == "chisq":
+            return self.__get_chisq(num_events)
+
+    def __get_chisq(self, num_events):
+        """
+        Generate a number of chi-squared type events.
+        The inputs are drawn from iid gaussian distributions.
+        The class is the condition: sum of squared inputs >= threshold
+        :param num_events:  Int. Number of events to generate
+        :return: x, y. Numpy arrays of float/int
+        """
         nx = np.random.normal(0., 1., self.num_inputs * num_events)
         print nx.shape
         x = nx.reshape((num_events, self.num_inputs))
--- machine_decision_loops/EventSelector.py
+++ machine_decision_loops/EventSelector.py
@@ -2,17 +2,46 @@ import numpy as np
 
 
 class EventSelector:
+    """
+    Class which filters streams of events based on model scores. If a single event stream is
+    fed to the filter, we use a threshold on the model score for each event. if a number of
+    event streams and the corresponding models are fed to the filter, the event with the
+    highest weighted model score is selected for each round of events.
+    """
     def __init__(self, criterion='single_stream'):
+        """
+        Initializatio. Only the criterion is needed here.
+        :param criterion: String. Support for single streams and competing event streams.
+        :return: None
+        """
         self.criterion = criterion
         assert self.criterion in ('single_stream', 'competing_streams')
 
     def filter(self, xs, ys, models, **kwargs):
+        """
+        The main method of this class. Filters event streams. Applies to both single event streams
+        and competing event streams.
+        :param xs: List of numpy array. Each item in the list is a segment of the inputs of an event stream.
+        :param ys: List of numpy array. Each item in the list is a segment of the labels of an event stream.
+        :param models: List of trained sklearn classifier models. Each model corresponds to an event stream.
+        :param kwargs: Parameters required for single stream or multiple streams.
+        :return: (List of numpy array, List of numpy array).
+        """
         if self.criterion == 'single_stream':
             return self.__filter_single_stream(xs, ys, models, **kwargs)
         elif self.criterion == 'competing_streams':
             return self.__filter_multiple_streams(xs, ys, models, **kwargs)
 
     def __filter_single_stream(self, xs, ys, models, threshold=0.4):
+        """
+        Filter a single event stream.
+        :param xs: List of numpy array of length one. The item in the list is a segment of the inputs of an event stream.
+        :param ys: List of numpy array of length one. The item in the list is a segment of the labels of an event stream.
+        :param models: List of trained sklearn classifier models of length one . The model corresponds to the event stream.
+        :param threshold: Float. If model.predict_proba(x) >= threshold, event x and its label
+                          remain in the stream.
+        :return: Tuple of numpy arrays. The event inputs and the event labels.
+        """
         assert len(xs) == 1
         assert len(ys) == 1
         assert len(models) == 1  # list of trained scikit classifiers
@@ -24,6 +53,16 @@ class EventSelector:
         return x[ix_keep, ], y[ix_keep, ]
 
     def __filter_multiple_streams(self, xs, ys, models, event_gains=None):
+        """
+        Filter competing event streams. Events arrive in rounds, one event per stream. At each round,
+        a single event from one stream is selected. The critersion is: choose the event with the highest
+        weighted probability of being in class 1.
+        :param xs: List of numpy array. Each item in the list is a segment of the inputs of an event stream.
+        :param ys: List of numpy array. Each item in the list is a segment of the labels of an event stream.
+        :param models: List of trained sklearn classifier models. Each model corresponds to an event stream.
+        :param event_gains: List of float. Weights to use when selecting which event to select in each round.
+        :return: Tuple of lists of numpy arrays. The event inputs and the event labels for each stream.
+        """
         # event_gains is iterable of float
         assert len(xs) > 1
         assert len(xs) == len(models)
@@ -51,6 +90,9 @@ class EventSelector:
 
 
 class DummyModel:
+    """
+    A class with dummy model predictions for testing.
+    """
     def __init__(self, kind='uniform'):
         assert kind in ('uniform', 'logit_gaussian', 'parity_sum')
         self.kind = kind
@@ -60,23 +102,24 @@ class DummyModel:
         if self.kind == 'uniform':
             p = np.random.uniform(0., 1., x.shape[1])
             pp = 1. - p
-            return np.hstack((np.ndarray(shape=(3,1), buffer=p.T),
-                              np.ndarray(shape=(3,1), buffer=pp.T)))
+            return np.hstack((np.ndarray(shape=(3, 1), buffer=p.T),
+                              np.ndarray(shape=(3, 1), buffer=pp.T)))
         elif self.kind == 'logit_gaussian':
             p = 1./(1. + np.exp(-1.*np.random.normal(0., 1., x.shape[1])))
             pp = 1. - p
-            return np.hstack((np.ndarray(shape=(3,1), buffer=p.T),
-                              np.ndarray(shape=(3,1), buffer=pp.T)))
+            return np.hstack((np.ndarray(shape=(3, 1), buffer=p.T),
+                              np.ndarray(shape=(3, 1), buffer=pp.T)))
         elif self.kind == 'parity_sum':
             s = np.sum(x, axis=1)
             p = 1. * (s % 2) / s
             pp = 1. - p
-            return np.hstack((np.ndarray(shape=(3,1), buffer=p.T),
-                              np.ndarray(shape=(3,1), buffer=pp.T)))
+            return np.hstack((np.ndarray(shape=(3, 1), buffer=p.T),
+                              np.ndarray(shape=(3, 1), buffer=pp.T)))
         else:
             return 0., 0.
 
 if __name__ == "__main__":
+    """Set of tests for the EventSelector class"""
     x1 = np.array([[0, 1, 0], [1, 2, 0], [1, 2, 3]])
     y1 = np.array([0.1, 0.2, 0.3])
     x2 = np.array([[0, 1, 1], [1, 2, 1], [2, 2, 3]])
